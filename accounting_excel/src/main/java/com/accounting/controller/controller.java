d1091cb5c1628c707107367e1f0d2e7875c1f1de
sungkyun <mskyun721@gamil.com>
2020-06-30 19:33:10 +0900

    06/30 19:32 문성균  - security 설정 / BeanFactory not initialized or already closed - call 'refresh' before accessing beans via the ApplicationContext 오류

--- accounting_excel/src/main/java/com/accounting/controller/controller.java
+++ accounting_excel/src/main/java/com/accounting/controller/controller.java
@@ -1,5 +1,26 @@
 package com.accounting.controller;
 
-public class controller {
+import javax.inject.Inject;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.accounting.service.testService;
+
+@Controller
+public class controller {
+	@Inject
+	testService testService;
+	
+	@RequestMapping(value="/login")
+	public String login(Model model,@RequestParam(value="error",required = false)String error) {
+		if (error != null) {
+			model.addAttribute("error", "아이디, 비밀번호를 확인하세요.");
+		}
+		
+		return "login";
+	}
+	
 }
